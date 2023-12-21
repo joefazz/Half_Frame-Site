9189fc7f1c4f438c31b6469829b4a492bdc5ac1e
Joe Fazzino <joe.fazzino@unity3d.com>
2023-12-21 15:00:56 +0000

    update shit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+
+import { Container } from '@/components/Container'
+import { FadeIn } from '@/components/FadeIn'
+import { Logo } from '@/components/Logo'
+import { socialMediaProfiles } from '@/components/SocialMedia'
+
+const navigation = [
+  {
+    title: 'Work',
+    links: [
+      { title: 'FamilyFund', href: '/work/family-fund' },
+      { title: 'Unseal', href: '/work/unseal' },
+      { title: 'Phobia', href: '/work/phobia' },
+      {
+        title: (
+          <>
+            See all <span aria-hidden="true">&rarr;</span>
+          </>
+        ),
+        href: '/work',
+      },
+    ],
+  },
+  {
+    title: 'Company',
+    links: [
+      { title: 'About', href: '/about' },
+      { title: 'Process', href: '/process' },
+      { title: 'Blog', href: '/blog' },
+      { title: 'Contact us', href: '/contact' },
+    ],
+  },
+  {
+    title: 'Connect',
+    links: socialMediaProfiles,
+  },
+]
+
+function Navigation() {
+  return (
+    <nav>
+      <ul role="list" className="grid grid-cols-2 gap-8 sm:grid-cols-3">
+        {navigation.map((section, sectionIndex) => (
+          <li key={sectionIndex}>
+            <div className="font-display text-sm font-semibold tracking-wider text-neutral-950">
+              {section.title}
+            </div>
+            <ul role="list" className="mt-4 text-sm text-neutral-700">
+              {section.links.map((link, linkIndex) => (
+                <li key={linkIndex} className="mt-4">
+                  <Link
+                    href={link.href}
+                    className="transition hover:text-neutral-950"
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export function Footer() {
+  return (
+    <Container as="footer" className="mt-24 w-full sm:mt-32 lg:mt-40">
+      <FadeIn>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
+          <div className="flex lg:justify-end">
+            {/* <Navigation /> */}
+          </div>
+        </div>
+        <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
+          <Link href="/" aria-label="Home">
+            <Logo className="h-8" fillOnHover />
+          </Link>
+          <p className="text-sm text-neutral-700">
+            © Half_Frame {new Date().getFullYear()}
+          </p>
+        </div>
+      </FadeIn>
+    </Container>
+  )
+}
